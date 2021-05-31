0c537009c5cdd8419ba6cfa73ddf0010184ab0d8
Nazar Kudatsky <divipi1996@icloud.com>
2021-05-31 12:55:08 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2088c32903613710f8f9678a01b40cd4.js"
+  "./precache-manifest.fa9ae6d6538b8b879b61f2c4562cbe00.js"
 );
 
 self.addEventListener('message', (event) => {
